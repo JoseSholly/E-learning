b8ef50e2ecc70f2c48c1801fce62c86ac0e64cec
JoseSholly <josepholawuyi30@gmail.com>
2024-02-22 10:50:01 +0100

    Adding student registration

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(),name= 'logout'),
     path('admin/', admin.site.urls),
     path('', CourseListView.as_view(), name= 'course_list'),
+    path('students/', include('students.urls')),
     path('course/', include('courses.urls'))
 ]
 
