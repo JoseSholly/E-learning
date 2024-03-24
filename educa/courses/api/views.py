db080c1bb9ab792502739c68de6ad926aa853c16
JoseSholly <josepholawuyi30@gmail.com>
2024-03-25 00:32:49 +0100

    Add: Additional actions to viewsets

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
 class SubjectListView(generics.ListAPIView):
     queryset= Subject.objects.all()
@@ -26,3 +27,11 @@ class CourseEnrollView(APIView):
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset= Course.objects.all()
     serializer_class= CourseSerializer
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes= [BasicAuthentication],
+            permission_classes= [IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course= self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
